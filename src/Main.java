fb729cb06de0de944447e7d88407387727f2e6a0
dantedevfr <dante.dev.fr@gmail.com>
2025-06-07 18:31:35 +0200

    adding files

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        String name = "Dante";
+        String nameComplet = name.concat("Other value");
+        System.out.println(nameComplet);
+    }
+}
